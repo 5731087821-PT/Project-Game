9a016c259329bee6bdf43d3b400bd2d54828df7d
Thuchchai Jiamsorn <naki_war@windowslive.com>
2015-12-11 00:02:11 +0700

    update template

--- src/utility/Resource.java
+++ src/utility/Resource.java
@@ -21,13 +21,20 @@ public class Resource {
 			ClassLoader loader = RenderableHolder.class.getClassLoader();
 			appleSprite = ImageIO.read(loader.getResource("res/Apple.png"));
 			coinSound = Applet.newAudioClip(Resource.class.getClassLoader().getResource("res/coin.wav"));
-			acShoot = Applet.newAudioClip(AudioUtility.class.getClassLoader().getResource("res/se/shoot.wav"));
-			acCollect = Applet.newAudioClip(AudioUtility.class.getClassLoader().getResource("res/se/collect.wav"));
+			acShoot = Applet.newAudioClip(Resource.class.getClassLoader().getResource("res/se/shoot.wav"));
+			acCollect = Applet.newAudioClip(Resource.class.getClassLoader().getResource("res/se/collect.wav"));
 		} catch (Exception e) {
 			appleSprite = null;
 			coinSound = null;
 			acShoot = null;
 			acCollect = null;
+		}
+	}
+	public static void playSound(String identifier){
+		if(identifier=="shoot"){
+			acShoot.play();
+		}else{
+			acCollect.play();
 		}
 	}
 }
